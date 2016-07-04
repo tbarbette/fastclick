c311fe9d23f4d728650630ffa7f9094c1a3af596
Romain Gaillard <rgaillard@student.ulg.ac.be>
2016-07-04 14:56:13 +0200

    (Test without TCPRetransmitter)

--- elements/middlebox/fcb.hh
+++ elements/middlebox/fcb.hh
@@ -138,7 +138,7 @@ struct fcb_tcpout
         closingState = TCPClosingState::OPEN;
     }
 };
-
+/*
 struct fcb_tcpretransmitter
 {
     rb_red_blk_tree* tree;
@@ -154,7 +154,7 @@ struct fcb_tcpretransmitter
         RBTreeDestroy(tree);
     }
 };
-
+*/
 struct fcb
 {
     struct fcb_tcp_common* tcp_common;
@@ -164,7 +164,7 @@ struct fcb
     struct fcb_httpin httpin;
     struct fcb_pathmerger pathmerger;
     struct fcb_tcpout tcpout;
-    struct fcb_tcpretransmitter tcpretransmitter;
+    //struct fcb_tcpretransmitter tcpretransmitter;
 
     fcb()
     {
