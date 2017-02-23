6f1bd6e3fe7f9c8ea173d0d6bc59f9ed6e18acfc
Georgios Katsikas <katsikas@kth.se>
2017-02-23 15:32:26 +0100

    First, spaces with tab length 4 are ensured

--- elements/ethernet/arpprint.hh
+++ elements/ethernet/arpprint.hh
@@ -50,34 +50,35 @@ Returns or sets the ACTIVE parameter.
 
 =a Print, CheckARPHeader */
 
-class ARPPrint : public Element { public:
+class ARPPrint : public Element {
+    public:
 
-    ARPPrint() CLICK_COLD;
-    ~ARPPrint() CLICK_COLD;
+        ARPPrint() CLICK_COLD;
+        ~ARPPrint() CLICK_COLD;
 
-    const char *class_name() const		{ return "ARPPrint"; }
-    const char *port_count() const		{ return PORTS_1_1; }
+        const char *class_name() const      { return "ARPPrint"; }
+        const char *port_count() const      { return PORTS_1_1; }
 
-    int configure(Vector<String> &, ErrorHandler *) CLICK_COLD;
-    int initialize(ErrorHandler *) CLICK_COLD;
-    void cleanup(CleanupStage) CLICK_COLD;
-    void add_handlers() CLICK_COLD;
+        int configure(Vector<String> &, ErrorHandler *) CLICK_COLD;
+        int initialize(ErrorHandler *) CLICK_COLD;
+        void cleanup(CleanupStage) CLICK_COLD;
+        void add_handlers() CLICK_COLD;
 
-    Packet *simple_action(Packet *);
+        Packet      *simple_action      (Packet *);
 
- private:
 
-    String _label;
-    bool _print_timestamp;
-    bool _print_ether;
-    bool _active;
+    private:
 
-#if CLICK_USERLEVEL
-    String _outfilename;
-    FILE *_outfile;
-#endif
-    ErrorHandler *_errh;
+        String _label;
+        bool _print_timestamp;
+        bool _print_ether;
+        bool _active;
 
+        #if CLICK_USERLEVEL
+        String _outfilename;
+        FILE *_outfile;
+        #endif
+        ErrorHandler *_errh;
 };
 
 CLICK_ENDDECLS
