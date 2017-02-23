ed07b1dcf609b24bf9edcbff06185e7355da81fb
Georgios Katsikas <katsikas@kth.se>
2017-02-23 09:11:39 +0100

    Converted tabs to spaces for ToDPDKRing and FromDPDKRing.hh

--- elements/userlevel/fromdpdkring.hh
+++ elements/userlevel/fromdpdkring.hh
@@ -79,46 +79,46 @@ Returns the number of bytes read from the ring.
 
 class FromDPDKRing : public BatchElement {
 
-	public:
-		FromDPDKRing () CLICK_COLD;
-		~FromDPDKRing() CLICK_COLD;
+    public:
+        FromDPDKRing () CLICK_COLD;
+        ~FromDPDKRing() CLICK_COLD;
 
-		const char    *class_name() const { return "FromDPDKRing"; }
-		const char    *port_count() const { return PORTS_0_1; }
-		const char    *processing() const { return PUSH; }
-		int       configure_phase() const { return CONFIGURE_PHASE_PRIVILEGED - 5; }
-		bool can_live_reconfigure() const { return false; }
+        const char    *class_name() const { return "FromDPDKRing"; }
+        const char    *port_count() const { return PORTS_0_1; }
+        const char    *processing() const { return PUSH; }
+        int       configure_phase() const { return CONFIGURE_PHASE_PRIVILEGED - 5; }
+        bool can_live_reconfigure() const { return false; }
 
-		int  configure   (Vector<String> &, ErrorHandler *)	CLICK_COLD;
-		int  initialize  (ErrorHandler *) 			CLICK_COLD;
-		void add_handlers()					CLICK_COLD;
-		void cleanup     (CleanupStage) 			CLICK_COLD;
+        int  configure   (Vector<String> &, ErrorHandler *) CLICK_COLD;
+        int  initialize  (ErrorHandler *)           CLICK_COLD;
+        void add_handlers()                 CLICK_COLD;
+        void cleanup     (CleanupStage)             CLICK_COLD;
 
-		// Calls either push_packet or push_batch
-		bool run_task    (Task *);
+        // Calls either push_packet or push_batch
+        bool run_task    (Task *);
 
-	private:
-		Task _task;
+    private:
+        Task _task;
 
-		struct rte_mempool *_message_pool;
-		struct rte_ring    *_recv_ring;
+        struct rte_mempool *_message_pool;
+        struct rte_ring    *_recv_ring;
 
-		String _MEM_POOL;
-		String _PROC_1;
-		String _PROC_2;
-		String _origin;
-		String _destination;
+        String _MEM_POOL;
+        String _PROC_1;
+        String _PROC_2;
+        String _origin;
+        String _destination;
 
-		unsigned     _ndesc;
-		unsigned     _burst_size;
-		unsigned     _def_burst_size;
-		unsigned int _iqueue_size;
-		short        _numa_zone;
+        unsigned     _ndesc;
+        unsigned     _burst_size;
+        unsigned     _def_burst_size;
+        unsigned int _iqueue_size;
+        short        _numa_zone;
 
-		counter_t    _pkts_recv;
-		counter_t    _bytes_recv;
+        counter_t    _pkts_recv;
+        counter_t    _bytes_recv;
 
-		static String read_handler(Element*, void*) CLICK_COLD;
+        static String read_handler(Element*, void*) CLICK_COLD;
 };
 
 CLICK_ENDDECLS
