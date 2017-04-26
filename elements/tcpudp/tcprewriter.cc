be21698ef2e2c77753cf5b4e481e237749d530be
Georgios Katsikas <george.dit@gmail.com>
2017-04-26 21:34:36 +0200

    Thread-safe IPRewriters. Most of them also support batching (except for ICMP)

--- elements/tcpudp/tcprewriter.cc
+++ elements/tcpudp/tcprewriter.cc
@@ -3,9 +3,12 @@
  * tcprewriter.{cc,hh} -- rewrites packet source and destination
  * Eddie Kohler
  *
+ * Per-core, thread safe data structures and batching by Georgios Katsikas
+ *
  * Copyright (c) 2000 Massachusetts Institute of Technology
  * Copyright (c) 2000 Mazu Networks, Inc.
  * Copyright (c) 2008-2010 Meraki, Inc.
+ * Copyright (c) 2016 KTH Royal Institute of Technology
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -236,10 +239,19 @@ TCPRewriter::TCPFlow::unparse(StringAccum &sa, bool direction, click_jiffies_t n
 
 TCPRewriter::TCPRewriter()
 {
+#if HAVE_USER_MULTITHREAD
+    _maps_no = ( click_max_cpu_ids() == 0 )? 1 : click_max_cpu_ids();
+    _allocator = new SizedHashAllocator<sizeof(TCPFlow)>[_maps_no];
+    //click_chatter("[%s]: Allocated %d TCP maps", class_name(), _maps_no);
+#endif
 }
 
 TCPRewriter::~TCPRewriter()
 {
+#if HAVE_USER_MULTITHREAD
+    if ( _allocator )
+        delete [] _allocator;
+#endif
 }
 
 void *
@@ -256,16 +268,19 @@ TCPRewriter::cast(const char *n)
 int
 TCPRewriter::configure(Vector<String> &conf, ErrorHandler *errh)
 {
+    uint32_t timeouts[2];
+
     // numbers in seconds
-    _timeouts[0] = 300;		// nodata: 5 minutes (should be > TCP_DONE)
+    timeouts[0] = 300;		// nodata: 5 minutes (should be > TCP_DONE)
+
     _tcp_data_timeout = 86400;	// 24 hours
     _tcp_done_timeout = 240;	// 4 minutes
     bool dst_anno = true, has_reply_anno = false;
     int reply_anno;
 
     if (Args(this, errh).bind(conf)
-	.read("TCP_NODATA_TIMEOUT", SecondsArg(), _timeouts[0])
-	.read("TCP_GUARANTEE", SecondsArg(), _timeouts[1])
+	.read("TCP_NODATA_TIMEOUT", SecondsArg(), timeouts[0])
+	.read("TCP_GUARANTEE", SecondsArg(), timeouts[1])
 	.read("TIMEOUT", SecondsArg(), _tcp_data_timeout)
 	.read("TCP_TIMEOUT", SecondsArg(), _tcp_data_timeout)
 	.read("TCP_DONE_TIMEOUT", SecondsArg(), _tcp_done_timeout)
@@ -273,6 +288,10 @@ TCPRewriter::configure(Vector<String> &conf, ErrorHandler *errh)
 	.read("REPLY_ANNO", AnnoArg(1), reply_anno).read_status(has_reply_anno)
 	.consume() < 0)
 	return -1;
+
+    for (unsigned i=0; i<_mem_units_no; i++) {
+        _timeouts[i] = timeouts;
+    }
 
     _annos = (dst_anno ? 1 : 0) + (has_reply_anno ? 2 + (reply_anno << 2) : 0);
     _tcp_data_timeout *= CLICK_HZ; // IPRewriterBase handles the others
@@ -286,20 +305,25 @@ TCPRewriter::add_flow(int /*ip_p*/, const IPFlowID &flowid,
 		      const IPFlowID &rewritten_flowid, int input)
 {
     void *data;
-    if (!(data = _allocator.allocate()))
+    if (!(data = _allocator[click_current_cpu_id()].allocate()))
 	return 0;
 
     TCPFlow *flow = new(data) TCPFlow
 	(&_input_specs[input], flowid, rewritten_flowid,
-	 !!_timeouts[1], click_jiffies() + relevant_timeout(_timeouts));
+	 !!_timeouts[click_current_cpu_id()][1], click_jiffies() +
+         relevant_timeout(_timeouts[click_current_cpu_id()]));
 
-    return store_flow(flow, input, _map);
+    return store_flow(flow, input, _map[click_current_cpu_id()]);
 }
 
-void
-TCPRewriter::push(int port, Packet *p_in)
+int
+TCPRewriter::process(int port, Packet *p_in)
 {
     WritablePacket *p = p_in->uniqueify();
+    if (!p) {
+        return -1;
+    }
+
     click_ip *iph = p->ip_header();
 
     // handle non-first fragments
@@ -307,41 +331,118 @@ TCPRewriter::push(int port, Packet *p_in)
 	|| !IP_FIRSTFRAG(iph)
 	|| p->transport_length() < 8) {
 	const IPRewriterInput &is = _input_specs[port];
-	if (is.kind == IPRewriterInput::i_nochange)
-	    output(is.foutput).push(p);
-	else
-	    p->kill();
-	return;
+	if (is.kind == IPRewriterInput::i_nochange) {
+            return is.foutput;
+        }
+	return -1;
     }
 
     IPFlowID flowid(p);
-    IPRewriterEntry *m = _map.get(flowid);
+    IPRewriterEntry *m = _map[click_current_cpu_id()].get(flowid);
 
     if (!m) {			// create new mapping
 	IPRewriterInput &is = _input_specs.unchecked_at(port);
 	IPFlowID rewritten_flowid = IPFlowID::uninitialized_t();
+
 	int result = is.rewrite_flowid(flowid, rewritten_flowid, p);
-	if (result == rw_addmap)
+	if (result == rw_addmap) {
 	    m = TCPRewriter::add_flow(IP_PROTO_TCP, flowid, rewritten_flowid, port);
+        }
+
 	if (!m) {
-	    checked_output_push(result, p);
-	    return;
-	} else if (_annos & 2)
+	    return result;
+	} else if (_annos & 2) {
 	    m->flow()->set_reply_anno(p->anno_u8(_annos >> 2));
+        }
     }
 
     TCPFlow *mf = static_cast<TCPFlow *>(m->flow());
     mf->apply(p, m->direction(), _annos);
 
     click_jiffies_t now_j = click_jiffies();
-    if (_timeouts[1])
-	mf->change_expiry(_heap, true, now_j + _timeouts[1]);
+    if (_timeouts[click_current_cpu_id()][1])
+	mf->change_expiry(_heap[click_current_cpu_id()], true, now_j + _timeouts[click_current_cpu_id()][1]);
     else
-	mf->change_expiry(_heap, false, now_j + tcp_flow_timeout(mf));
+	mf->change_expiry(_heap[click_current_cpu_id()], false, now_j + tcp_flow_timeout(mf));
 
-    output(m->output()).push(p);
+    return m->output();
 }
 
+void
+TCPRewriter::push(int port, Packet *p)
+{
+    int output_port = process(port, p);
+    if ( output_port < 0 ) {
+        p->kill();
+        return;
+    }
+
+    output(output_port).push(p);
+}
+
+#if HAVE_BATCH
+void
+TCPRewriter::push_batch(int port, PacketBatch *batch)
+{
+    unsigned short outports = noutputs();
+    PacketBatch* out[outports];
+    bzero(out,sizeof(PacketBatch*)*outports);
+    PacketBatch *next = ((batch != NULL)? static_cast<PacketBatch*>(batch->next()) : NULL );
+    PacketBatch *p = batch;
+    PacketBatch *last = NULL;
+    int last_o = -1;
+    int passed = 0;
+    int count  = 0;
+    for (; p != NULL;p=next,next=(p==0?0:static_cast<PacketBatch*>(p->next()))) {
+        // The actual job of this element
+        int o = process(port, p);
+
+        if (o < 0 || o>=(outports)) {
+            o = (outports - 1);
+        }
+
+        if (o == last_o) {
+            passed ++;
+        }
+        else {
+            if (!last) {
+                out[o] = p;
+                p->set_count(1);
+                p->set_tail(p);
+            }
+            else {
+                out[last_o]->set_tail(last);
+                out[last_o]->set_count(out[last_o]->count() + passed);
+                if (!out[o]) {
+                    out[o] = p;
+                    out[o]->set_count(1);
+                    out[o]->set_tail(p);
+                }
+                else {
+                    out[o]->append_packet(p);
+                }
+                passed = 0;
+            }
+        }
+        last = p;
+        last_o = o;
+        count++;
+    }
+
+    if (passed) {
+        out[last_o]->set_tail(last);
+        out[last_o]->set_count(out[last_o]->count() + passed);
+    }
+
+    int i = 0;
+    for (; i < outports; i++) {
+        if (out[i]) {
+            out[i]->tail()->set_next(NULL);
+            checked_output_push_batch(i, out[i]);
+        }
+    }
+}
+#endif
 
 String
 TCPRewriter::tcp_mappings_handler(Element *e, void *)
@@ -349,7 +450,7 @@ TCPRewriter::tcp_mappings_handler(Element *e, void *)
     TCPRewriter *rw = (TCPRewriter *)e;
     click_jiffies_t now = click_jiffies();
     StringAccum sa;
-    for (Map::iterator iter = rw->_map.begin(); iter.live(); ++iter) {
+    for (Map::iterator iter = rw->_map[click_current_cpu_id()].begin(); iter.live(); ++iter) {
 	TCPFlow *f = static_cast<TCPFlow *>(iter->flow());
 	f->unparse(sa, iter->direction(), now);
 	sa << '\n';
