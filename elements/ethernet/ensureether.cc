fad76065f6c2ce68d4b396ff38a08af8c342cce4
Georgios Katsikas <katsikas.gp@gmail.com>
2018-09-23 15:23:49 +0200

    Batching support for EnsureEther
    Signed-off-by: Georgios Katsikas <katsikas.gp@gmail.com>

--- elements/ethernet/ensureether.cc
+++ elements/ethernet/ensureether.cc
@@ -38,61 +38,83 @@ EnsureEther::configure(Vector<String> &conf, ErrorHandler *errh)
     memset(&_ethh.ether_shost, 1, 6);
     memset(&_ethh.ether_dhost, 2, 6);
     if (Args(conf, this, errh)
-	.read_p("ETHERTYPE", etht)
-	.read_p("SRC", EtherAddressArg(), _ethh.ether_shost)
-	.read_p("DST", EtherAddressArg(), _ethh.ether_dhost)
-	.complete() < 0)
-	return -1;
+        .read_p("ETHERTYPE", etht)
+        .read_p("SRC", EtherAddressArg(), _ethh.ether_shost)
+        .read_p("DST", EtherAddressArg(), _ethh.ether_dhost)
+        .complete() < 0)
+        return -1;
+
     if (etht > 0xFFFF)
-	return errh->error("argument 1 (Ethernet encapsulation type) must be <= 0xFFFF");
+        return errh->error("argument 1 (Ethernet encapsulation type) must be <= 0xFFFF");
+
     _ethh.ether_type = htons(etht);
+
     return 0;
 }
 
 Packet *
 EnsureEther::smaction(Packet *p)
 {
-  if (!p->has_network_header() || p->ip_header_offset() < 0)
-    return p;
+    if (!p->has_network_header() || p->ip_header_offset() < 0)
+        return p;
 
-  if (p->ip_header_offset() == 14) {
-    // check for an existing Ethernet header
-    const click_ether *ethh = (const click_ether *)p->data();
-    if (ethh->ether_type == htons(ETHERTYPE_IP)
-	|| ethh->ether_type == htons(ETHERTYPE_IP6))
-      return p;
-  } else if (p->ip_header_offset() == 0 && p->headroom() >= 14) {
-    // check for an Ethernet header that had been stripped
-    const click_ether *ethh = (const click_ether *)(p->data() - 14);
-    if (ethh->ether_type == htons(ETHERTYPE_IP)
-	|| ethh->ether_type == htons(ETHERTYPE_IP6))
-      return p->nonunique_push(14);
-  }
+    if (p->ip_header_offset() == 14) {
+        // check for an existing Ethernet header
+        const click_ether *ethh = (const click_ether *)p->data();
+        if (ethh->ether_type == htons(ETHERTYPE_IP)
+         || ethh->ether_type == htons(ETHERTYPE_IP6))
+            return p;
+    } else if (p->ip_header_offset() == 0 && p->headroom() >= 14) {
+        // check for an Ethernet header that had been stripped
+        const click_ether *ethh = (const click_ether *)(p->data() - 14);
+        if (ethh->ether_type == htons(ETHERTYPE_IP)
+         || ethh->ether_type == htons(ETHERTYPE_IP6))
+            return p->nonunique_push(14);
+    }
 
-  // need to prepend our own Ethernet header
-  p->pull(p->ip_header_offset());
-  if (WritablePacket *q = p->push(14)) {
-    memcpy(q->data(), &_ethh, 14);
-    return q;
-  } else
-    return 0;
+    // need to prepend our own Ethernet header
+    p->pull(p->ip_header_offset());
+    if (WritablePacket *q = p->push(14)) {
+        memcpy(q->data(), &_ethh, 14);
+        return q;
+    } else
+        return 0;
 }
 
 void
 EnsureEther::push(int, Packet *p)
 {
-  if (Packet *q = smaction(p))
-    output(0).push(q);
+    if (Packet *q = smaction(p))
+        output(0).push(q);
 }
 
 Packet *
 EnsureEther::pull(int)
 {
-  if (Packet *p = input(0).pull())
-    return smaction(p);
-  else
-    return 0;
+    if (Packet *p = input(0).pull())
+        return smaction(p);
+    else
+        return 0;
 }
+
+#if HAVE_BATCH
+void
+EnsureEther::push_batch(int, PacketBatch *batch)
+{
+    EXECUTE_FOR_EACH_PACKET_DROPPABLE(smaction, batch, [](Packet *){});
+    if (batch)
+        output(0).push_batch(batch);
+}
+
+PacketBatch *
+EnsureEther::pull_batch(int port, unsigned max)
+{
+    PacketBatch *batch;
+    MAKE_BATCH(EnsureEther::pull(port), batch, max);
+    return batch;
+}
+
+#endif
 
 CLICK_ENDDECLS
 EXPORT_ELEMENT(EnsureEther)
