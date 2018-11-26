tbarbette/fastclick

commit 4bed2b3dd73864078d63d9bf89c0b4f7c0a4a7aa

From*Device: Add SCALE parameter
If parallel, all available threads will open one queue of each device,
for better load balancing.
If shared (current default and behaviour), threads will be split among
devices, for better CPU locality.

elements/userlevel/tonetmapdevice.cc
@@ -44,9 +44,9 @@ ToNetmapDevice::configure(Vector<String> &conf, ErrorHandler *errh)
     String ifname;
     int burst = -1;
 
-    if (parse(Args(conf, this, errh)
-    .read_mp("DEVNAME", ifname), errh)
-    .complete() < 0)
+    if (Args(this, errh).bind(conf)
+            .read_mp("DEVNAME", ifname)
+            .complete() < 0)
     	return -1;
 
     if (_internal_tx_queue_size < _burst * 2) {

elements/userlevel/fromdpdkdevice.cc
@@ -56,8 +56,15 @@ int FromDPDKDevice::configure(Vector<String> &conf, ErrorHandler *errh)
     bool has_mtu = false;
     FlowControlMode fc_mode(FC_UNSET);
 
-    if (parse(Args(conf, this, errh)
-        .read_mp("PORT", dev))
+    if (Args(this, errh).bind(conf)
+        .read_mp("PORT", dev)
+        .consume() < 0)
+        return -1;
+
+    if (parse(conf, errh) != 0)
+        return -1;
+
+    if (Args(conf, this, errh)
         .read("NDESC", ndesc)
         .read("MAC", mac).read_status(has_mac)
         .read("MTU", mtu).read_status(has_mtu)

elements/userlevel/fromnetmapdevice.cc
@@ -57,10 +57,17 @@ FromNetmapDevice::configure(Vector<String> &conf, ErrorHandler *errh)
 
 	int thisnode = 0;
 
-    if (parse(Args(conf, this, errh)
-    .read_mp("DEVNAME", ifname))
-  	.read("KEEPHAND",_keephand)
-  	.complete() < 0)
+	if (Args(this, errh).bind(conf)
+            .read_mp("DEVNAME", ifname)
+            .consume() < 0)
+        return -1;
+	int err = parse(conf, errh);
+	if (err != 0)
+		return err;
+
+    if (Args(conf, this, errh)
+		.read("KEEPHAND",_keephand)
+		.complete() < 0)
     	return -1;
 #if HAVE_NUMA
     if (_use_numa) {

elements/userlevel/queuedevice.cc
@@ -46,25 +46,32 @@ void QueueDevice::static_initialize() {
     shared_offset.fill(0);
 }
 
-Args& QueueDevice::parse(Args &args) {
-    args.read_p("QUEUE", firstqueue)
+int QueueDevice::parse(Vector<String> &conf, ErrorHandler *errh) {
+
+    if (Args(this, errh).bind(conf)
+            .read_p("QUEUE", firstqueue)
             .read("N_QUEUES",n_queues)
             .read("MAXTHREADS", _maxthreads)
             .read("BURST", _burst)
             .read("VERBOSE", _verbose)
             .read("ACTIVE", _active)
-            .read("ALLOW_NONEXISTENT", allow_nonexistent);
+            .read("ALLOW_NONEXISTENT", allow_nonexistent)
+            .consume() < 0)
+        return -1;
 
     n_elements ++;
 
-    return args;
+    return 0;
 }
 
-Args& RXQueueDevice::parse(Args &args) {
-	args = QueueDevice::parse(args);
+int RXQueueDevice::parse(Vector<String> &conf, ErrorHandler *errh) {
+	QueueDevice::parse(conf, errh);
 
 	_promisc = true;
-	args.read_p("PROMISC", _promisc);
+    if (Args(this, errh).bind(conf)
+	.read_p("PROMISC", _promisc)
+        .consume() < 0)
+        return -1;
 
 #if HAVE_NUMA
 	_use_numa = true;
@@ -74,11 +81,28 @@ Args& RXQueueDevice::parse(Args &args) {
 	_threadoffset = -1;
 	_set_rss_aggregate = false;
 	_set_paint_anno = false;
+	String scale;
+	bool has_scale = false;
+	_scale_parallel = false;
 
-	args.read("RSS_AGGREGATE", _set_rss_aggregate)
+    if (Args(this, errh).bind(conf)
+            .read("RSS_AGGREGATE", _set_rss_aggregate)
             .read("PAINT_QUEUE", _set_paint_anno)
             .read("NUMA", _use_numa)
-            .read("THREADOFFSET", _threadoffset);
+			.read("SCALE", scale).read_status(has_scale)
+            .read("THREADOFFSET", _threadoffset)
+            .consume() < 0)
+        return -1;
+
+	if (has_scale) {
+		if (scale.lower() == "parallel") {
+			_scale_parallel = true;
+		} else if (scale.lower() == "share") {
+			_scale_parallel = false;
+		} else {
+			return errh->error("Unknown scaling mode %s !",scale.c_str());
+		}
+	}
 
 #if !HAVE_NUMA
 	if (_use_numa) {
@@ -86,17 +110,20 @@ Args& RXQueueDevice::parse(Args &args) {
 	}
 	_use_numa = false;
 #endif
-	return args;
+	return 0;
 }
 
-Args& TXQueueDevice::parse(Args &args, ErrorHandler* errh) {
-        QueueDevice::parse(args);
-        args.read("IQUEUE", _internal_tx_queue_size)
-            .read("BLOCKING", _blocking);
+int TXQueueDevice::parse(Vector<String> &conf, ErrorHandler* errh) {
+        QueueDevice::parse(conf, errh);
+        Args args(this, errh);
+        if (args.bind(conf).read("IQUEUE", _internal_tx_queue_size)
+            .read("BLOCKING", _blocking)
+            .consume() < 0)
+            return -1;
         if ((_internal_tx_queue_size & (_internal_tx_queue_size - 1)) != 0) {
             errh->error("IQUEUE must be a power of 2");
         }
-        return args;
+        return 0;
 }
 
 int RXQueueDevice::configure_rx(int numa_node, int minqueues, int maxqueues, ErrorHandler *) {
@@ -237,7 +264,10 @@ int RXQueueDevice::initialize_rx(ErrorHandler *errh) {
 
        //click_chatter("_maxthreads %d, cores_in_node %d, nthreads() %d, use_nodes %d, _this_node %d, inputs_count %d",_maxthreads,cores_in_node,master()->nthreads(),use_nodes,_this_node,inputs_count[_this_node]);
        if (_maxthreads == -1) {
-           n_threads = min(cores_in_node,master()->nthreads() / use_nodes) / inputs_count[_this_node];
+           if (_scale_parallel)
+               n_threads = min(cores_in_node,master()->nthreads() / use_nodes);
+           else
+               n_threads = min(cores_in_node,master()->nthreads() / use_nodes) / inputs_count[_this_node];
        } else {
            n_threads = min(cores_in_node,_maxthreads);
        }
@@ -251,7 +281,10 @@ int RXQueueDevice::initialize_rx(ErrorHandler *errh) {
                if (use_nodes > 1)
                    use_nodes = 1;
            }
-           thread_share = inputs_count[_this_node] / min(cores_in_node,master()->nthreads() / use_nodes);
+           if (!_scale_parallel)
+		   thread_share = inputs_count[_this_node] / min(cores_in_node,master()->nthreads() / use_nodes);
+           else
+		   thread_share = min(cores_in_node,master()->nthreads());
        }
 
        if (n_threads > _maxqueues) {
@@ -259,15 +292,20 @@ int RXQueueDevice::initialize_rx(ErrorHandler *errh) {
        }
 
        if (_threadoffset == -1) {
-           _threadoffset = shared_offset[_this_node];
-           shared_offset[_this_node] += n_threads;
+           if (!_scale_parallel) {
+               _threadoffset = shared_offset[_this_node];
+               shared_offset[_this_node] += n_threads;
+           } else {
+                _threadoffset = 0;
+           }
        }
 
        if (thread_share > 1) {
            if (_threadoffset != -1) {
                errh->warning("Thread offset %d will be ignored because the numa node has not enough cores.",_threadoffset);
            }
-           _threadoffset = _threadoffset % (inputs_count[_this_node] / thread_share);
+           if (!_scale_parallel)
+		   _threadoffset = _threadoffset % (inputs_count[_this_node] / thread_share);
        } else
            if (n_threads + _threadoffset > master()->nthreads())
                _threadoffset = master()->nthreads() - n_threads;

elements/userlevel/queuedevice.hh
@@ -152,7 +152,7 @@ protected:
     /**
      * Common parsing for all kind of QueueDevice
      */
-    Args& parse(Args &args);
+    int parse(Vector<String> &conf, ErrorHandler *errh);
 
 
     bool all_initialized() {
@@ -228,11 +228,12 @@ protected:
 	bool _set_paint_anno;
 	int _threadoffset;
 	bool _use_numa;
+	bool _scale_parallel;
 
     /**
      * Common parsing for all RXQueueDevice
      */
-    Args& parse(Args &args);
+	int parse(Vector<String> &conf, ErrorHandler *errh);
 
     /*
      * Configure a RX side of a queuedevice. Take cares of setting user max
@@ -253,7 +254,7 @@ protected:
     /**
      * Common parsing for all RXQueueDevice
      */
-    Args& parse(Args &args, ErrorHandler* errh);
+    int parse(Vector<String> &conf, ErrorHandler *errh);
 
     int configure_tx(int hardminqueues, int hardmaxqueues, ErrorHandler *errh);
     int initialize_tx(ErrorHandler *errh);

elements/userlevel/todpdkdevice.cc
@@ -42,9 +42,16 @@ int ToDPDKDevice::configure(Vector<String> &conf, ErrorHandler *errh)
 {
     int maxqueues = 128;
     String dev;
+    if (Args(conf, this, errh)
+            .read_mp("PORT", dev)
+            .consume() < 0)
+        return -1;
 
-    if (parse(Args(conf, this, errh)
-        .read_mp("PORT", dev), errh)
+
+    if (parse(conf, errh) != 0)
+        return -1;
+
+    if (Args(conf, this, errh)
         .read("TIMEOUT", _timeout)
         .read("NDESC",ndesc)
         .read("MAXQUEUES", maxqueues)

commit 5084abf1259f7a2fe77a8553a7e27ae5f0cb5e11

Fix bug introduced by "From*Device: Add SCALE parameter"
Fixes bug introduced by 4bed2b3dd73864078d63d9bf89c0b4f7c0a4a7aa

elements/userlevel/tonetmapdevice.cc
@@ -46,8 +46,11 @@ ToNetmapDevice::configure(Vector<String> &conf, ErrorHandler *errh)
 
     if (Args(this, errh).bind(conf)
             .read_mp("DEVNAME", ifname)
-            .complete() < 0)
+            .consume() < 0)
     	return -1;
+
+    if (parse(conf, errh) != 0)
+        return -1;
 
     if (_internal_tx_queue_size < _burst * 2) {
         return errh->error("IQUEUE (%d) must be at least twice the size of BURST (%d)!",_internal_tx_queue_size, _burst);