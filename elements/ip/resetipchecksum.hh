1bfbd5ef2f92e2501069b9e79757f13f2adb42ec
Tom Barbette <barbette@kth.se>
2019-06-23 10:05:45 +0200

    ResetIPChecksum: Reset TCP/IP checksum and asks hardware to fix it

---
+++ elements/ip/resetipchecksum.hh
@@ -0,0 +1,40 @@
+#ifndef CLICK_RESETIPCHECKSUM_HH
+#define CLICK_RESETIPCHECKSUM_HH
+#include <click/batchelement.hh>
+#include <click/glue.hh>
+CLICK_DECLS
+
+/*
+ * =c
+ * ResetIPChecksum()
+ * =s ip
+ * sets IP packets' checksums
+ * =d
+ * Expects an IP packet as input.
+ * 
+ * Resets the IP checksum and asks hardware to fix it
+ * 
+ * =a CheckIPHeader, DecIPTTL, ResetIPDSCP, IPRewriter */
+
+class ResetIPChecksum : public BatchElement { public:
+
+    ResetIPChecksum() CLICK_COLD;
+    ~ResetIPChecksum() CLICK_COLD;
+
+    const char *class_name() const		{ return "ResetIPChecksum"; }
+    const char *port_count() const		{ return PORTS_1_1; }
+    void add_handlers() CLICK_COLD;
+
+    Packet *simple_action(Packet *p);
+#if HAVE_BATCH
+    PacketBatch *simple_action_batch(PacketBatch *);
+#endif
+
+  private:
+
+    unsigned _drops;
+
+};
+
+CLICK_ENDDECLS
+#endif
