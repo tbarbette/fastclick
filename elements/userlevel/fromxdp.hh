024a4e540957f6d487fc0a56de2b62fe046114ef
Ryan Goodfellow <rgoodfel@isi.edu>
2019-08-06 23:14:48 -0400

    first crack at FromXDP device
    Signed-off-by: Ryan Goodfellow <rgoodfel@isi.edu>

---
+++ elements/userlevel/fromxdp.hh
@@ -0,0 +1,109 @@
+#pragma once
+#include <click/config.h>
+#include <click/batchelement.hh>
+
+#ifndef AF_XDP
+#define AF_XDP 44
+#endif
+
+#ifndef SOL_XDP
+#define SOL_XDP 283
+#endif
+
+#define barrier() __asm__ __volatile__("": : :"memory")
+#define u_smp_wmb() barrier()
+#define u_smp_rmb() barrier()
+
+#define NUM_FRAMES 131072
+#define NUM_DESCS 1024
+#define FRAME_SIZE 2048
+#define FRAME_HEADROOM 0
+#define FRAME_TAILROOM 0
+#define FQ_NUM_DESCS 1024
+#define CQ_NUM_DESCS 1024
+#define BATCH_SIZE 16
+
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+struct xdp_uqueue {
+	u32 cached_prod;
+	u32 cached_cons;
+	u32 mask;
+	u32 size;
+	u32 *producer;
+	u32 *consumer;
+	struct xdp_desc *ring;
+	void *map;
+};
+
+struct xdpsock {
+	struct xdp_uqueue rx;
+	struct xdp_uqueue tx;
+	int sfd;
+	struct xdp_umem *umem;
+	u32 outstanding_tx;
+	unsigned long rx_npkts;
+	unsigned long tx_npkts;
+	unsigned long prev_rx_npkts;
+	unsigned long prev_tx_npkts;
+};
+
+struct xdp_umem_uqueue {
+	u32 cached_prod;
+	u32 cached_cons;
+	u32 mask;
+	u32 size;
+	u32 *producer;
+	u32 *consumer;
+	u64 *ring;
+	void *map;
+};
+
+struct xdp_umem {
+	char *frames;
+	struct xdp_umem_uqueue fq;
+	struct xdp_umem_uqueue cq;
+	int fd;
+};
+
+CLICK_DECLS
+
+class FromXDP : public BatchElement {
+
+  public:
+
+    FromXDP() CLICK_COLD = default;
+    ~FromXDP() CLICK_COLD = default;
+
+    int configure(Vector<String>&, ErrorHandler*) override final CLICK_COLD;
+    int initialize(ErrorHandler*) override final CLICK_COLD;
+
+    const char *class_name() const override final { return "FromXDP"; }
+    const char *port_count() const override final { return "0/1"; }
+    const char *processing() const override final { return PUSH; }
+
+    bool run_task(Task *t) override final;
+
+  private:
+
+    String _dev;
+    String _mode;
+
+    struct bpf_map *_map;
+    xdpsock *_xsk;
+
+    int _xsk_map;
+    u32 _ifx_index{0};
+    u16 _flags{0};
+    u16 _bind_flags{0};
+    int _sfd;
+
+    void set_rlimit(ErrorHandler*);
+    void init_bpf(ErrorHandler*);
+    void init_xsk(ErrorHandler*);
+
+};
+
+CLICK_ENDDECLS
