0b387e22a66603232e2fd79294643d820b057981
Tom Barbette <barbette@kth.se>
2019-10-24 11:27:47 +0200

    (Un)Strip transport header
    Allows to move the data pointer up to the TCP or UDP payload

---
+++ elements/tcpudp/unstriptransportheader.hh
@@ -0,0 +1,40 @@
+#ifndef CLICK_UnstripTransportHeader_HH
+#define CLICK_UnstripTransportHeader_HH
+#include <click/batchelement.hh>
+CLICK_DECLS
+
+/*
+ * =c
+ * UnstripTransportHeader()
+ * =s ip
+ * restores outermost transport header
+ * =d
+ *
+ * Put outermost transport header back onto a stripped packet, based
+ * on the transport header annotation from, e.g. MarkIPHeader or CheckIPHeader.
+ * If transport header already on forwards packet unmodified.
+ *
+ * Beware that this element will not "undo" StripTransportHeader.
+ * StripTransportHeader will jump from anywhere to the first byte of payload
+ * after the transport header. This element will return to the byte before the
+ * transport header.
+ *
+ * =a CheckIPHeader, MarkIPHeader, StripIPHeader, StripTransportHeader */
+
+class UnstripTransportHeader : public BatchElement {
+
+public:
+  UnstripTransportHeader() CLICK_COLD;
+  ~UnstripTransportHeader() CLICK_COLD;
+
+  const char *class_name() const		{ return "UnstripTransportHeader"; }
+  const char *port_count() const		{ return PORTS_1_1; }
+
+  Packet      *simple_action      (Packet      *p);
+#if HAVE_BATCH
+  PacketBatch *simple_action_batch(PacketBatch *batch);
+#endif
+};
+
+CLICK_ENDDECLS
+#endif
