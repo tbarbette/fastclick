6396a39840e758653edce39abe810bb0f13dbdd6
Tom Barbette <barbette@kth.se>
2019-10-24 11:24:27 +0200

    DiscardNoFreeBuffer: Discard packets, but do not free the buffer
    The difference with discardnofree is that the packet object is recycled.
    
    This is useful in combination with DPDK and Replay variants with
    QuickClone. As the buffer is silently re-used.

---
+++ elements/standard/discardnofreebuffer.hh
@@ -0,0 +1,42 @@
+#ifndef CLICK_DISCARDNOFREEBUFFER_HH
+#define CLICK_DISCARDNOFREEBUFFER_HH
+#include <click/batchelement.hh>
+#include <click/task.hh>
+CLICK_DECLS
+
+/*
+ * =c
+ * DiscardNoFreeBuffer
+ * =s basicsources
+ * drops all packets, but does not free their buffers. The packet
+ * descriptor (Packet object) is recycled though.
+ * =d
+ * Discards all packets received on its single input, but does not free their
+ * buffer. Only useful for benchmarking or in combination to DPDK and Replay
+ * (with QUICK_CLONE) for packet generator.
+ */
+
+class DiscardNoFreeBuffer : public BatchElement { public:
+
+  DiscardNoFreeBuffer() CLICK_COLD;
+
+  const char *class_name() const		{ return "DiscardNoFreeBuffer"; }
+  const char *port_count() const		{ return PORTS_1_0; }
+
+  int initialize(ErrorHandler *) CLICK_COLD;
+  void add_handlers() CLICK_COLD;
+
+  void push(int, Packet *);
+#if HAVE_BATCH
+  void push_batch(int, PacketBatch *);
+#endif
+  bool run_task(Task *);
+
+ private:
+
+  Task _task;
+
+};
+
+CLICK_ENDDECLS
+#endif
