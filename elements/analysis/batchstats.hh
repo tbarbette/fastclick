dd5a0cc4746890f1cacec38c842aa8fa2354d540
Tom Barbette <barbette@kth.se>
2020-04-06 14:50:15 +0200

    Fix batchstats

--- elements/analysis/batchstats.hh
+++ elements/analysis/batchstats.hh
@@ -4,6 +4,7 @@
 #include <click/batchelement.hh>
 #include <click/multithread.hh>
 #include <click/vector.hh>
+#include <click/statvector.hh>
 CLICK_DECLS
 
 /*
@@ -23,13 +24,14 @@ handlers
 
  */
 
-class BatchStats : public BatchElement { public:
+class BatchStats : public BatchElement, StatVector<int> { public:
 
     BatchStats() CLICK_COLD;
     ~BatchStats() CLICK_COLD;
 
     const char *class_name() const	{ return "BatchStats"; }
     const char *port_count() const	{ return PORTS_1_1; }
+    void * cast(const char *name);
 
     int configure(Vector<String> &, ErrorHandler *) CLICK_COLD;
     int initialize(ErrorHandler *) CLICK_COLD;
@@ -41,11 +43,7 @@ class BatchStats : public BatchElement { public:
 #endif
 
     void add_handlers();
-private:
 
-    per_thread_omem<Vector<int>> stats;
-    enum{H_MEDIAN,H_AVERAGE,H_DUMP};
-    static String read_handler(Element *e, void *thunk);
 };
 
 CLICK_ENDDECLS
