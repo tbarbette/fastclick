730c78673d30e85f577afde85cc59dc30a393855
Tom Barbette <barbette@kth.se>
2020-04-24 14:29:00 +0200

    More style fixes

--- elements/research/randload.cc
+++ elements/research/randload.cc
@@ -1,5 +1,18 @@
-/*
- * RandLoad.{cc,hh}
+/ -*- c-basic-offset: 4 -*-
+/* randload.{cc,hh} --
+ * Tom Barbette
+ *
+ * Copyright (c) 2020 KTH Royal Insitute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
  */
 
 #include "randload.hh"
@@ -36,9 +49,9 @@ int RandLoad::initialize(ErrorHandler *errh) {
 }
 
 void RandLoad::push_batch(int port, PacketBatch* batch) {
-	int r;
+    int r;
     auto fnt = [this,&r](Packet* p)  {
-    	int w =  _min + ((*_gens)() / (UINT_MAX / (_max - _min) ));
+        int w =  _min + ((*_gens)() / (UINT_MAX / (_max - _min) ));
         for (int i = 0; i < w - 1; i ++) {
             r = (*_gens)();
         }
