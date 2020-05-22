9a9eb00211957dd0f32d1d90e5d7259534cb78a1
Tom Barbette <barbette@kth.se>
2020-05-22 15:29:32 +0200

    LinkUnqueue: Support batch mode

--- elements/standard/linkunqueue.cc
+++ elements/standard/linkunqueue.cc
@@ -32,6 +32,7 @@ CLICK_DECLS
 LinkUnqueue::LinkUnqueue()
     : _qhead(0), _qtail(0), _task(this), _timer(&_task)
 {
+    in_batch_mode = BATCH_MODE_YES;
 }
 
 void *
@@ -117,41 +118,58 @@ LinkUnqueue::run_task(Task *)
 
     // Read a new packet if there's room.  Room is measured by the latency
     while (!_qtail || _qtail->timestamp_anno() <= now_delayed) {
-	// try to pull a packet
-	Packet *p = input(0).pull();
-	if (!p) {
-	    _back_to_back = false;
-	    break;
-	}
+        // try to pull a packet
+        Packet *p = input(0).pull();
+        if (!p) {
+            _back_to_back = false;
+            break;
+        }
 
-	// set new timestamp to delayed timestamp
-	if (_qtail) {
-	    _qtail->set_next(p);
-	    delay_by_bandwidth(p, (_back_to_back ? _qtail->timestamp_anno() : now_delayed));
-	} else {
-	    _qhead = p;
-	    delay_by_bandwidth(p, now_delayed);
-	}
+        // set new timestamp to delayed timestamp
+        if (_qtail) {
+            _qtail->set_next(p);
+            delay_by_bandwidth(p, (_back_to_back ? _qtail->timestamp_anno() : now_delayed));
+        } else {
+            _qhead = p;
+            delay_by_bandwidth(p, now_delayed);
+        }
 
-	// hook up, and remember we were doing this back to back
-	_qtail = p;
-	p->set_next(0);
-	Storage::set_tail(Storage::tail() + 1);
-	worked = _back_to_back = true;
+        // hook up, and remember we were doing this back to back
+        _qtail = p;
+        p->set_next(0);
+        Storage::set_tail(Storage::tail() + 1);
+        worked = _back_to_back = true;
     }
 
+  #if HAVE_BATCH
+    BATCH_CREATE_INIT(ready_batch);
+  #endif
     // Emit packets if it's time
     while (_qhead && _qhead->timestamp_anno() <= now) {
-	Packet *p = _qhead;
-	_qhead = p->next();
-	if (!_qhead)
-	    _qtail = 0;
-	p->set_next(0);
-	//click_chatter("%p{timestamp}: RELEASE %p{timestamp}", &now, &p->timestamp_anno());
-	output(0).push(p);
-	Storage::set_tail(Storage::tail() - 1);
-	worked = true;
+        Packet *p = _qhead;
+        _qhead = p->next();
+        if (!_qhead)
+            _qtail = 0;
+        p->set_next(0);
+        //click_chatter("%p{timestamp}: RELEASE %p{timestamp}", &now, &p->timestamp_anno());
+#if HAVE_BATCH
+        if (likely(in_batch_mode)) {
+            BATCH_CREATE_APPEND(ready_batch, p);
+        } else
+#endif
+        {
+            output(0).push(p);
+        }
+        Storage::set_tail(Storage::tail() - 1);
+        worked = true;
     }
+
+#if HAVE_BATCH
+    BATCH_CREATE_FINISH(ready_batch);
+    if (ready_batch) {
+        output_push_batch(0, ready_batch);
+    }
+#endif
 
     // Figure out when to schedule next
     //print_queue(_qhead);
@@ -181,6 +199,13 @@ LinkUnqueue::run_task(Task *)
     }
 
     return worked;
+}
+
+bool
+LinkUnqueue::get_spawning_threads(Bitvector& b, bool, int port) {
+    unsigned int thisthread = router()->home_thread_id(this);
+    b[thisthread] = 1;
+    return false;
 }
 
 enum { H_LATENCY, H_BANDWIDTH, H_SIZE, H_RESET };
