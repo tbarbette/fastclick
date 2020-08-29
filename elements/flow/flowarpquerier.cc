e96e7329daa4ee1053921044d4dabca95c6d0ac8
Tom Barbette <barbette@kth.se>
2020-08-29 20:34:57 +0200

    Add missing element

---
+++ elements/flow/flowarpquerier.cc
@@ -0,0 +1,474 @@
+/*
+ * flowarpquerier.{cc,hh} -- ARP resolver element
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include "flowarpquerier.hh"
+#include <clicknet/ether.h>
+#include <click/etheraddress.hh>
+#include <click/ipaddress.hh>
+#include <click/args.hh>
+#include <click/bitvector.hh>
+#include <click/straccum.hh>
+#include <click/router.hh>
+#include <click/error.hh>
+#include <click/glue.hh>
+#include <click/packet_anno.hh>
+CLICK_DECLS
+
+FlowARPQuerier::FlowARPQuerier()
+    : _arpt(0), _my_arpt(false), _zero_warned(false)
+{
+}
+
+FlowARPQuerier::~FlowARPQuerier()
+{
+}
+
+void *
+FlowARPQuerier::cast(const char *name)
+{
+    if (strcmp(name, "ARPTable") == 0)
+	return _arpt;
+    else if (strcmp(name, "FlowARPQuerier") == 0)
+	return this;
+    else
+	return FlowSpaceElement<fcb_arpcache>::cast(name);
+}
+
+int
+FlowARPQuerier::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+    uint32_t capacity, entry_capacity, entry_packet_capacity, capacity_slim_factor;
+    Timestamp timeout, poll_timeout(60);
+    bool have_capacity, have_entry_capacity, have_entry_packet_capacity, have_capacity_slim_factor, have_timeout, have_broadcast,
+	broadcast_poll = false;
+    _arpt = 0;
+    if (Args(this, errh).bind(conf)
+	.read("CAPACITY", capacity).read_status(have_capacity)
+	.read("ENTRY_CAPACITY", entry_capacity).read_status(have_entry_capacity)
+	.read("ENTRY_PACKET_CAPACITY", entry_packet_capacity).read_status(have_entry_packet_capacity)
+	.read("CAPACITY_SLIM_FACTOR", capacity_slim_factor).read_status(have_capacity_slim_factor)
+	.read("TIMEOUT", timeout).read_status(have_timeout)
+	.read("BROADCAST", _my_bcast_ip).read_status(have_broadcast)
+	.read("TABLE", ElementCastArg("ARPTable"), _arpt)
+	.read("POLL_TIMEOUT", poll_timeout)
+	.read("BROADCAST_POLL", broadcast_poll)
+	.consume() < 0)
+	return -1;
+
+    if (!_arpt) {
+	Vector<String> subconf;
+	if (have_capacity)
+	    subconf.push_back("CAPACITY " + String(capacity));
+	if (have_entry_capacity)
+	    subconf.push_back("ENTRY_CAPACITY " + String(entry_capacity));
+	if (have_entry_packet_capacity)
+	    subconf.push_back("ENTRY_PACKET_CAPACITY " + String(entry_packet_capacity));
+	if (have_capacity_slim_factor)
+	    subconf.push_back("CAPACITY_SLIM_FACTOR " + String(capacity_slim_factor));
+	if (have_timeout)
+	    subconf.push_back("TIMEOUT " + timeout.unparse());
+	_arpt = new ARPTable;
+	_arpt->attach_router(router(), -1);
+	_arpt->configure(subconf, errh);
+	_my_arpt = true;
+    }
+    _arpt->add_remote_element(this);
+
+    IPAddress my_mask;
+    if (conf.size() == 1)
+	conf.push_back(conf[0]);
+    if (Args(conf, this, errh)
+	.read_mp("IP", IPPrefixArg(true), _my_ip, my_mask)
+	.read_mp("ETH", _my_en)
+	.complete() < 0)
+	return -1;
+
+    if (!have_broadcast) {
+	_my_bcast_ip = _my_ip | ~my_mask;
+	if (_my_bcast_ip == _my_ip)
+	    _my_bcast_ip = 0xFFFFFFFFU;
+    }
+
+    _broadcast_poll = broadcast_poll;
+    if ((uint32_t) poll_timeout.sec() >= (uint32_t) 0xFFFFFFFFU / CLICK_HZ)
+	_poll_timeout_j = 0;
+    else
+	_poll_timeout_j = poll_timeout.jiffies();
+
+    return 0;
+}
+
+int
+FlowARPQuerier::live_reconfigure(Vector<String> &conf, ErrorHandler *errh)
+{
+    uint32_t capacity, entry_capacity, entry_packet_capacity, capacity_slim_factor;
+    Timestamp timeout, poll_timeout(Timestamp::make_jiffies((click_jiffies_t) _poll_timeout_j));
+    bool have_capacity, have_entry_capacity, have_entry_packet_capacity, have_capacity_slim_factor, have_timeout, have_broadcast,
+	broadcast_poll(_broadcast_poll);
+    IPAddress my_bcast_ip;
+
+    if (Args(this, errh).bind(conf)
+	.read("CAPACITY", capacity).read_status(have_capacity)
+	.read("ENTRY_CAPACITY", entry_capacity).read_status(have_entry_capacity)
+	.read("ENTRY_PACKET_CAPACITY", entry_packet_capacity).read_status(have_entry_packet_capacity)
+	.read("CAPACITY_SLIM_FACTOR", capacity_slim_factor).read_status(have_capacity_slim_factor)
+	.read("TIMEOUT", timeout).read_status(have_timeout)
+	.read("BROADCAST", my_bcast_ip).read_status(have_broadcast)
+	.read_with("TABLE", AnyArg())
+	.read("POLL_TIMEOUT", poll_timeout)
+	.read("BROADCAST_POLL", broadcast_poll)
+	.consume() < 0)
+	return -1;
+
+    IPAddress my_ip, my_mask;
+    EtherAddress my_en;
+    if (conf.size() == 1)
+	conf.push_back(conf[0]);
+    if (Args(conf, this, errh)
+	.read_mp("IP", IPPrefixArg(true), my_ip, my_mask)
+	.read_mp("ETH", my_en)
+	.complete() < 0)
+	return -1;
+    if (!have_broadcast) {
+	my_bcast_ip = my_ip | ~my_mask;
+	if (my_bcast_ip == my_ip)
+	    my_bcast_ip = 0xFFFFFFFFU;
+    }
+    if (have_capacity_slim_factor && !capacity_slim_factor)
+        return errh->error("CAPACITY_SLIM_FACTOR cannot be zero");
+
+    if ((my_ip != _my_ip || my_en != _my_en) && _my_arpt)
+	_arpt->clear();
+
+    _my_ip = my_ip;
+    _my_en = my_en;
+    _my_bcast_ip = my_bcast_ip;
+    if (_my_arpt && have_capacity)
+	_arpt->set_capacity(capacity);
+    if (_my_arpt && have_entry_capacity)
+	_arpt->set_entry_capacity(entry_capacity);
+    if (_my_arpt && have_entry_packet_capacity)
+	_arpt->set_entry_packet_capacity(entry_packet_capacity);
+    if (_my_arpt && have_capacity_slim_factor)
+	_arpt->set_capacity_slim_factor(capacity_slim_factor);
+    if (_my_arpt && have_timeout)
+	_arpt->set_timeout(timeout);
+
+    _broadcast_poll = broadcast_poll;
+    if ((uint32_t) poll_timeout.sec() >= (uint32_t) 0xFFFFFFFFU / CLICK_HZ)
+	_poll_timeout_j = 0;
+    else
+	_poll_timeout_j = poll_timeout.jiffies();
+
+    return 0;
+}
+
+int
+FlowARPQuerier::initialize(ErrorHandler *errh)
+{
+    _arp_queries = 0;
+    _drops = 0;
+    _arp_responses = 0;
+    if (_my_arpt)
+        return _arpt->initialize(errh);
+    return FlowSpaceElement<fcb_arpcache>::initialize(errh);
+}
+
+void
+FlowARPQuerier::cleanup(CleanupStage stage)
+{
+    if (_my_arpt) {
+	_arpt->cleanup(stage);
+	delete _arpt;
+    }
+}
+
+void
+FlowARPQuerier::take_state(Element *e, ErrorHandler *errh)
+{
+    FlowARPQuerier *arpq = (FlowARPQuerier *) e->cast("FlowARPQuerier");
+    if (!arpq || _my_ip != arpq->_my_ip || _my_en != arpq->_my_en
+	|| _my_bcast_ip != arpq->_my_bcast_ip)
+	return;
+
+    if (_my_arpt && arpq->_my_arpt)
+	_arpt->take_state(arpq->_arpt, errh);
+    _arp_queries = arpq->_arp_queries;
+    _drops = arpq->_drops;
+    _arp_responses = arpq->_arp_responses;
+}
+
+void
+FlowARPQuerier::send_query_for(const Packet *p, bool ether_dhost_valid)
+{
+    // Uses p's IP and Ethernet headers.
+
+    static_assert(Packet::default_headroom >= sizeof(click_ether), "Packet::default_headroom must be at least 14.");
+    WritablePacket *q = Packet::make(Packet::default_headroom - sizeof(click_ether),
+				     NULL, sizeof(click_ether) + sizeof(click_ether_arp), 0);
+    if (!q) {
+	click_chatter("in arp querier: cannot make packet!");
+	return;
+    }
+
+    click_ether *e = (click_ether *) q->data();
+    q->set_ether_header(e);
+    if (ether_dhost_valid && likely(!_broadcast_poll))
+	memcpy(e->ether_dhost, p->ether_header()->ether_dhost, 6);
+    else
+	memset(e->ether_dhost, 0xff, 6);
+    memcpy(e->ether_shost, _my_en.data(), 6);
+    e->ether_type = htons(ETHERTYPE_ARP);
+
+    click_ether_arp *ea = (click_ether_arp *) (e + 1);
+    ea->ea_hdr.ar_hrd = htons(ARPHRD_ETHER);
+    ea->ea_hdr.ar_pro = htons(ETHERTYPE_IP);
+    ea->ea_hdr.ar_hln = 6;
+    ea->ea_hdr.ar_pln = 4;
+    ea->ea_hdr.ar_op = htons(ARPOP_REQUEST);
+    memcpy(ea->arp_sha, _my_en.data(), 6);
+    memcpy(ea->arp_spa, _my_ip.data(), 4);
+    memset(ea->arp_tha, 0, 6);
+    IPAddress want_ip = p->dst_ip_anno();
+    memcpy(ea->arp_tpa, want_ip.data(), 4);
+
+    q->set_timestamp_anno(p->timestamp_anno());
+    SET_VLAN_TCI_ANNO(q, VLAN_TCI_ANNO(p));
+
+    _arp_queries++;
+#if HAVE_BATCH
+    if (in_batch_mode == BATCH_MODE_YES) {
+        output(noutputs() - 1).push_batch(PacketBatch::make_from_packet(q));
+    } else
+#endif
+        output(noutputs() - 1).push(q);
+}
+
+/*
+ * If the packet's IP address is in the table, add an ethernet header
+ * and push it out.
+ * Otherwise push out a query packet.
+ * May save the packet in the ARP table for later sending.
+ * May call p->kill().
+ */
+Packet*
+FlowARPQuerier::handle_ip(Packet *p, fcb_arpcache* fcb, bool response)
+{
+    // delete packet if we are not configured
+    if (!_my_ip) {
+	p->kill();
+	++_drops;
+	return 0;
+    }
+
+    // make room for Ethernet header
+    WritablePacket *q;
+    if (response) {
+	assert(!p->shared());
+	q = p->uniqueify();
+    } else if (!(q = p->push_mac_header(sizeof(click_ether)))) {
+	++_drops;
+	return 0;
+    } else
+	q->ether_header()->ether_type = htons(ETHERTYPE_IP);
+
+    EtherAddress *dst_eth = reinterpret_cast<EtherAddress *>(q->ether_header()->ether_dhost);
+
+    /*if (fcb && fcb->dst) {
+        *dst_eth = fcb->dst;
+        memcpy(&q->ether_header()->ether_shost, _my_en.data(), 6);
+        return q;
+    }*/
+
+    IPAddress dst_ip = q->dst_ip_anno();
+    int r;
+
+    // Easy case: requires only read lock
+  retry_read_lock:
+    r = _arpt->lookup(dst_ip, dst_eth, _poll_timeout_j);
+    if (r >= 0) {
+	assert(!dst_eth->is_broadcast());
+	if (r > 0) {
+        SFCB_STACK(
+	    send_query_for(q, true);
+        );
+    }
+	// ... and send packet below.
+    } else if (dst_ip.addr() == 0xFFFFFFFFU || dst_ip == _my_bcast_ip) {
+	memset(dst_eth, 0xff, 6);
+	// ... and send packet below.
+    } else if (dst_ip.is_multicast()) {
+	uint8_t *dst_addr = q->ether_header()->ether_dhost;
+	dst_addr[0] = 0x01;
+	dst_addr[1] = 0x00;
+	dst_addr[2] = 0x5E;
+	uint32_t addr = ntohl(dst_ip.addr());
+	dst_addr[3] = (addr >> 16) & 0x7F;
+	dst_addr[4] = addr >> 8;
+	dst_addr[5] = addr;
+	// ... and send packet below.
+    } else {
+	// Zero or unknown address: do not send the packet.
+	if (!dst_ip) {
+	    if (!_zero_warned) {
+		click_chatter("%s: would query for 0.0.0.0; missing dest IP addr annotation?", declaration().c_str());
+		_zero_warned = true;
+	    }
+	    ++_drops;
+	    q->kill();
+	} else {
+	    r = _arpt->append_query(dst_ip, q);
+	    if (r == -EAGAIN)
+		goto retry_read_lock;
+	    if (r < 0)
+		q->kill();
+	    if (r > 0) {
+            SFCB_STACK(
+		send_query_for(q, false); // q is on the ARP entry's queue
+            )
+        }
+	    // if r >= 0, do not q->kill() since it is stored in some ARP entry.
+	}
+	return 0;
+    }
+
+    if (fcb)
+        fcb->dst = *dst_eth;
+
+    // It's time to emit the packet with our Ethernet address as source.  (Set
+    // the source address immediately before send in case the user changes the
+    // source address while packets are enqueued.)
+    memcpy(&q->ether_header()->ether_shost, _my_en.data(), 6);
+    return q;
+}
+
+/*
+ * Got an ARP response.
+ * Update our ARP table.
+ * If there was a packet waiting to be sent, return it.
+ */
+void
+inline FlowARPQuerier::handle_response(Packet *p)
+{
+    if (p->length() < sizeof(click_ether) + sizeof(click_ether_arp))
+	return;
+
+    ++_arp_responses;
+
+    click_ether *ethh = (click_ether *) p->data();
+    click_ether_arp *arph = (click_ether_arp *) (ethh + 1);
+    IPAddress ipa = IPAddress(arph->arp_spa);
+    EtherAddress ena = EtherAddress(arph->arp_sha);
+    if (ntohs(ethh->ether_type) == ETHERTYPE_ARP
+	&& ntohs(arph->ea_hdr.ar_hrd) == ARPHRD_ETHER
+	&& ntohs(arph->ea_hdr.ar_pro) == ETHERTYPE_IP
+	&& ntohs(arph->ea_hdr.ar_op) == ARPOP_REPLY
+	&& !ena.is_group()) {
+        Packet *cached_packet;
+        _arpt->insert(ipa, ena, &cached_packet);
+
+        // Send out packets in the order in which they arrived
+            BATCH_CREATE_INIT(batch_to_send);
+            while (cached_packet) {
+                Packet *next = cached_packet->next();
+                Packet* to_send = handle_ip(cached_packet, 0, true);
+                if (to_send) {
+                    BATCH_CREATE_APPEND(batch_to_send, to_send);
+                }
+                cached_packet = next;
+            }
+            BATCH_CREATE_FINISH(batch_to_send);
+            if (batch_to_send)
+                output(0).push_batch(batch_to_send);
+        }
+}
+
+void
+FlowARPQuerier::push_batch(int port, fcb_arpcache* fcb, PacketBatch *batch)
+{
+    if (port == 0) {
+        auto fnt = [&fcb,this](Packet*p){return handle_ip(p,fcb);};
+        EXECUTE_FOR_EACH_PACKET_DROPPABLE(fnt,batch,[](Packet*){});
+        if (batch)
+            output(0).push_batch(batch);
+    } else {
+        FOR_EACH_PACKET_SAFE(batch,p) {
+            SFCB_STACK(
+            handle_response(p);
+            );
+            p->kill();
+        }
+    }
+}
+
+String
+FlowARPQuerier::read_handler(Element *e, void *thunk)
+{
+    FlowARPQuerier *q = (FlowARPQuerier *)e;
+    switch (reinterpret_cast<uintptr_t>(thunk)) {
+    case h_table:
+	return q->_arpt->read_handler(q->_arpt, (void *) (uintptr_t) ARPTable::h_table);
+    case h_stats:
+	return
+	    String(q->_drops.value() + q->_arpt->drops()) + " packets killed\n" +
+	    String(q->_arp_queries.value()) + " ARP queries sent\n";
+    case h_count:
+	return String(q->_arpt->count());
+    case h_length:
+	return String(q->_arpt->length());
+    default:
+	return String();
+    }
+}
+
+int
+FlowARPQuerier::write_handler(const String &str, Element *e, void *thunk, ErrorHandler *errh)
+{
+    FlowARPQuerier *q = (FlowARPQuerier *) e;
+    switch (reinterpret_cast<uintptr_t>(thunk)) {
+    case h_insert:
+	return q->_arpt->write_handler(str, q->_arpt, (void *) (uintptr_t) ARPTable::h_insert, errh);
+    case h_delete:
+	return q->_arpt->write_handler(str, q->_arpt, (void *) (uintptr_t) ARPTable::h_delete, errh);
+    case h_clear:
+	q->_arp_queries = q->_drops = q->_arp_responses = 0;
+	q->_arpt->clear();
+	return 0;
+    default:
+	return -1;
+    }
+}
+
+void
+FlowARPQuerier::add_handlers()
+{
+    add_read_handler("table", read_handler, h_table);
+    add_read_handler("stats", read_handler, h_stats);
+    add_read_handler("count", read_handler, h_count);
+    add_read_handler("length", read_handler, h_length);
+    add_data_handlers("queries", Handler::OP_READ, &_arp_queries);
+    add_data_handlers("responses", Handler::OP_READ, &_arp_responses);
+    add_data_handlers("drops", Handler::OP_READ, &_drops);
+    add_data_handlers("broadcast", Handler::OP_READ | Handler::OP_WRITE, &_my_bcast_ip);
+    add_data_handlers("ipaddr", Handler::OP_READ | Handler::OP_WRITE, &_my_ip);
+    add_write_handler("insert", write_handler, h_insert);
+    add_write_handler("delete", write_handler, h_delete);
+    add_write_handler("clear", write_handler, h_clear);
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(FlowARPQuerier)
+ELEMENT_REQUIRES(ARPTable)
+ELEMENT_MT_SAFE(FlowARPQuerier)
---
+++ elements/flow/flowarpquerier.hh
@@ -0,0 +1,239 @@
+#ifndef CLICK_FLOWARPQUERIER_HH
+#define CLICK_FLOWARPQUERIER_HH
+#include <click/etheraddress.hh>
+#include <click/ipaddress.hh>
+#include <click/sync.hh>
+#include <click/timer.hh>
+
+#include <click/flow/flowelement.hh>
+#include "../ethernet/arptable.hh"
+CLICK_DECLS
+
+struct fcb_arpcache {
+    fcb_arpcache() : dst() {
+    }
+    EtherAddress dst;
+};
+
+/*
+=c
+
+FlowARPQuerier(IP, ETH, I<keywords>)
+FlowARPQuerier(NAME, I<keywords>)
+
+=s arp
+
+encapsulates IP packets in Ethernet headers found via ARP
+
+=d
+
+Handles most of the ARP protocol. Argument IP should be
+this host's IP address, and ETH should be this host's
+Ethernet address. (In
+the one-argument form, NAME should be shorthand for
+both an IP and an Ethernet address; see AddressInfo(n).)
+
+Packets arriving on input 0 should be IP packets, and must have their
+destination address annotations set.
+If an Ethernet address is already known
+for the destination, the IP packet is wrapped in an Ethernet
+header and sent to output 0. Otherwise the IP packet is saved and
+an ARP query is sent instead. If an ARP response arrives
+on input 1 for an IP address that we need, the mapping is
+recorded and any saved IP packets are sent.
+
+The ARP reply packets on input 1 should include the Ethernet header.
+
+FlowARPQuerier may have one or two outputs. If it has two, then ARP queries
+are sent to the second output.
+
+FlowARPQuerier implements special behavior for 0.0.0.0, 255.255.255.255, multicast
+addresses, and, if specified, any BROADCAST address.  Packets addressed to
+0.0.0.0 are dropped.  Packets for broadcast addresses are forwarded with
+destination Ethernet address FF-FF-FF-FF-FF-FF.  Multicast IP addresses are
+forwarded to 01-00-5E-xx-yy-zz, where xx-yy-zz are the lower 23 bits of the
+multicast IP address, as specified in RFC1112.
+
+Keyword arguments are:
+
+=over 8
+
+=item TABLE
+
+Element.  Names an ARPTable element that holds this element's corresponding
+ARP state.  By default FlowARPQuerier creates its own internal ARPTable and uses
+that.  If TABLE is specified, CAPACITY, ENTRY_CAPACITY, ENTRY_PACKET_CAPACITY,
+and TIMEOUT are ignored.
+
+=item CAPACITY
+
+Unsigned integer.  The maximum number of saved IP packets the table will
+hold at a time.  Default is 2048.
+
+=item ENTRY_CAPACITY
+
+Unsigned integer.  The maximum number of ARP entries the table will hold
+at a time.  Default is 0, which means unlimited.
+
+=item ENTRY_PACKET_CAPACITY
+
+Unsigned integer.  The maximum number of saved packets the table will hold
+for any given ARP entry at a time.  Default is 0, which means unlimited.
+
+=item TIMEOUT
+
+Amount of time before an ARP entry expires.  Defaults to 5 minutes.
+
+=item POLL_TIMEOUT
+
+Amount of time after which FlowARPQuerier will start polling for renewal.  0 means
+don't poll.  Defaults to one minute.
+
+=item BROADCAST
+
+IP address.  Local broadcast IP address.  Packets sent to this address will be
+forwarded to Ethernet address FF-FF-FF-FF-FF-FF.  Defaults to the local
+broadcast address that can be extracted from the IP address's corresponding
+prefix, if any.
+
+=item BROADCAST_POLL
+
+Boolean.  If true, then send broadcast ARP polls (where an entry is about to
+expire, but hasn't expired yet).  The default is to send such polls unicast to
+the known Ethernet address.  Defaults to false.
+
+=back
+
+=e
+
+   c :: Classifier(12/0806 20/0002, 12/0800, ...);
+   a :: FlowARPQuerier(18.26.4.24, 00:00:C0:AE:67:EF);
+   c[0] -> a[1];
+   c[1] -> ... -> a[0];
+   a[0] -> ... -> ToDevice(eth0);
+
+=n
+
+If a host has multiple interfaces, it will need multiple
+instances of FlowARPQuerier.
+
+FlowARPQuerier uses packets' destination IP address annotations, and can destroy
+their next packet annotations.  Generated ARP queries have VLAN TCI
+annotations set from the corresponding input packets.
+
+FlowARPQuerier will send at most 10 queries a second for any IP address.
+
+=h ipaddr rw
+
+Returns or sets the FlowARPQuerier's source IP address.
+
+=h broadcast r
+
+Returns the FlowARPQuerier's IP broadcast address.
+
+=h table r
+
+Returns a textual representation of the ARP table.  See ARPTable's table
+handler.
+
+=h stats r
+
+Returns textual statistics (queries and drops).
+
+=h queries r
+
+Returns the number of queries sent.
+
+=h responses r
+
+Returns the number of responses received.
+
+=h drops r
+
+Returns the number of packets dropped.
+
+=h count r
+
+Returns the number of entries in the ARP table.
+
+=h length r
+
+Returns the number of packets stored in the ARP table.
+
+=h insert w
+
+Add an entry to the ARP table.  The input string should have the form "IP ETH".
+
+=h delete w
+
+Delete an entry from the ARP table.  The input string should be an IP address.
+
+=h clear w
+
+Clear the ARP table.
+
+=a
+
+ARPTable, ARPResponder, ARPFaker, AddressInfo
+*/
+
+class FlowARPQuerier : public FlowSpaceElement<fcb_arpcache> { public:
+
+    FlowARPQuerier() CLICK_COLD;
+    ~FlowARPQuerier() CLICK_COLD;
+
+    const char *class_name() const		{ return "FlowARPQuerier"; }
+    const char *port_count() const		{ return "2/1-2"; }
+    const char *processing() const		{ return PUSH; }
+    const char *flow_code() const		{ return "xy/x"; }
+    // click-undead should consider all paths live (not just "xy/x"):
+    const char *flags() const			{ return "L2"; }
+    void *cast(const char *name);
+
+    FLOW_ELEMENT_DEFINE_PORT_SESSION_CONTEXT(1,"20/0002", FLOW_ARP);
+
+    int configure(Vector<String> &, ErrorHandler *) CLICK_COLD;
+    int live_reconfigure(Vector<String> &, ErrorHandler *);
+    bool can_live_reconfigure() const		{ return true; }
+    int initialize(ErrorHandler *errh) CLICK_COLD;
+    void add_handlers() CLICK_COLD;
+    void cleanup(CleanupStage stage) CLICK_COLD;
+    void take_state(Element *e, ErrorHandler *errh);
+
+    void push_batch(int port, fcb_arpcache* fcb, PacketBatch *batch);
+
+  private:
+
+    ARPTable *_arpt;
+    EtherAddress _my_en;
+    IPAddress _my_ip;
+    IPAddress _my_bcast_ip;
+    uint32_t _poll_timeout_j;
+    int _broadcast_poll;
+
+    // statistics
+    atomic_uint32_t _arp_queries;
+    atomic_uint32_t _drops;
+    atomic_uint32_t _arp_responses;
+    atomic_uint32_t _broadcasts;
+    bool _my_arpt;
+    bool _zero_warned;
+
+    void send_query_for(const Packet *p, bool ether_dhost_valid);
+
+    inline Packet* handle_ip(Packet *p, fcb_arpcache* fcb, bool response = false);
+    inline void handle_response(Packet *p);
+
+    static void expire_hook(Timer *, void *);
+    static String read_table(Element *, void *);
+    static String read_table_xml(Element *, void *);
+    static String read_handler(Element *, void *) CLICK_COLD;
+    static int write_handler(const String &, Element *, void *, ErrorHandler *) CLICK_COLD;
+
+    enum { h_table, h_table_xml, h_stats, h_insert, h_delete, h_clear,
+	   h_count, h_length };
+
+};
+
+CLICK_ENDDECLS
+#endif
