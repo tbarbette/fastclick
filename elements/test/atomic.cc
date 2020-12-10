3383d731308c61deb7bb8c07bd0acdd64dd6cd0c
Tom Barbette <barbette@kth.se>
2020-12-10 09:30:09 +0100

    Ignore warning in test

--- elements/test/atomic.cc
+++ elements/test/atomic.cc
@@ -35,10 +35,13 @@ static void test_32(ErrorHandler *errh)
     a=1;
     u = a.nonatomic_dec_and_test();
     click_chatter("[10] a=%i, u=%i -> %s", a, u,  a==0 && u==1 ? "PASS" : "FAIL");
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
     u = a.compare_and_swap(0,2);
     click_chatter("[11] a=%i, u=%i -> %s", a, u,  a==2 && u==1 ? "PASS" : "FAIL");
     u = a.compare_and_swap(0,5);
     click_chatter("[12] a=%i, u=%i -> %s", a, u,  a==2 && u==0 ? "PASS" : "FAIL");
+#pragma GCC diagnostic pop
     uint32_t b =5;
     u = atomic_uint32_t::swap(b,7);
     click_chatter("[13] b=%i, u=%i -> %s", b, u,  b==7 && u==5 ? "PASS" : "FAIL");
