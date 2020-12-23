6cc19380bcc2824ef1fef3a36ff5ba7cc939e740
Alireza Farshin <farshin@kth.se>
2020-12-23 11:07:43 +0000

    Some fixes in the WorkPackage to enable replacements

--- elements/research/workpackage.cc
+++ elements/research/workpackage.cc
@@ -36,16 +36,16 @@ WorkPackage::WorkPackage() : _w(1)
 int
 WorkPackage::configure(Vector<String> &conf, ErrorHandler *errh)
 {
-    double s;
+    double _s;
     if (Args(conf, this, errh)
-        .read_mp("S", s) //Array size in MB
-        .read_mp("N", _n) //Number of array access (4bytes)
-        .read_mp("R", _r) //Percentage of access that are packet read (vs Array access) between 0 and 100
-        .read_mp("PAYLOAD", _payload) //Access payload or only header
-        .read("W",_w) //Amount of call to random, purely CPU intensive fct
+        .read_or_set("S", _s, 0) //Array size in MB
+        .read_or_set("N", _n, 0) //Number of array access (4bytes)
+        .read_or_set("R", _r, 0) //Percentage of access that are packet read (vs Array access) between 0 and 100
+        .read_or_set("PAYLOAD", _payload, false) //Access payload or only header
+        .read_or_set("W",_w, 0) //Amount of call to random, purely CPU intensive fct
         .complete() < 0)
         return -1;
-    _array.resize(int(s * 1024 * 1024 / (float)sizeof(uint32_t)));
+    _array.resize(int(_s * 1024 * 1024 / (float)sizeof(uint32_t)));
     for (int i = 0; i < _array.size(); i++) {
         _array[i] = rd();
     }
