b45a8b83ee8216018341b19d0b9aafe0c5e2d498
Tom Barbette <barbette@kth.se>
2021-01-03 20:41:05 +0100

    Fix coverage failure

--- elements/test/atomic.cc
+++ elements/test/atomic.cc
@@ -44,20 +44,24 @@ static void test_32(ErrorHandler *errh)
 #pragma GCC diagnostic pop
     uint32_t b =5;
     u = atomic_uint32_t::swap(b,7);
-    click_chatter("[13] b=%i, u=%i -> %s", b, u,  b==7 && u==5 ? "PASS" : "FAIL");
+    click_chatter("[13] b=%i, u=%i -> %s", (uint32_t)b, u,  b==7 && u==5 ? "PASS" : "FAIL");
     atomic_uint32_t::inc(b);
-    click_chatter("[14] b=%i -> %s", b,  b==8 ? "PASS" : "FAIL");
+    click_chatter("[14] b=%i -> %s", (uint32_t)b,  b==8 ? "PASS" : "FAIL");
     u=atomic_uint32_t::dec_and_test(b);
-    click_chatter("[15] b=%i, u=%i -> %s", b, u,  b==7 && u==0 ? "PASS" : "FAIL");
+    click_chatter("[15] b=%i, u=%i -> %s", (uint32_t)b, u,  b==7 && u==0 ? "PASS" : "FAIL");
     b=1;
     u=atomic_uint32_t::dec_and_test(b);
-    click_chatter("[16] b=%i, u=%i -> %s", b, u,  b==0 && u==1 ? "PASS" : "FAIL");
+    click_chatter("[16] b=%i, u=%i -> %s", (uint32_t)b, u,  b==0 && u==1 ? "PASS" : "FAIL");
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
     b=5;
     u = atomic_uint32_t::compare_and_swap(b,0,2);
     click_chatter("[17] b=%i, u=%i -> %s", b, u,  b==5 && u==0 ? "PASS" : "FAIL");
     u = atomic_uint32_t::compare_and_swap(b,5,2);
     click_chatter("[18] b=%i, u=%i -> %s", b, u,  b==2 && u==1 ? "PASS" : "FAIL");
+#pragma GCC diagnostic pop
 
 #if CLICK_ATOMIC_BUILTINS
     click_chatter("[--] Not testing compare_swap!");
