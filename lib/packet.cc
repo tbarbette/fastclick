4af644caea634739589d0f62eb14e60f874df85a
Alireza Farshin <farshin@kth.se>
2021-02-10 17:15:51 +0100

    introduce --disable-pool-inlining to fix data structure reordering for X-Change

--- lib/packet.cc
+++ lib/packet.cc
@@ -280,7 +280,7 @@ static PacketPool global_packet_pool = {0,0,0,0};
 
 /** @brief Return the local packet pool for this thread.
     @pre initialize_local_packet_pool() has succeeded on this thread. */
-static CLICK_ALWAYS_INLINE inline PacketPool& local_packet_pool() {
+static CLICK_ALWAYS_INLINE PacketPool& local_packet_pool() {
 #  if HAVE_MULTITHREAD
     return *thread_packet_pool;
 #  else
@@ -348,7 +348,11 @@ WritablePacket::pool_batch_allocate(uint16_t count)
 /**
  * Allocate a batch of packets with buffer
  */
+#if POOL_INLINING
 CLICK_ALWAYS_INLINE WritablePacket *
+#else
+CLICK_OPTNONE WritablePacket *
+#endif
 WritablePacket::pool_prepare_data_burst(uint16_t count)
 {
         PacketPool& packet_pool = local_packet_pool();
