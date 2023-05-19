048dcdfa031180c610e45babf75119ebffb7d367
Tom Barbette <tom.barbette@uclouvain.be>
2023-05-19 08:23:20 +0200

    Deps: add dependency to perl Which package

--- deps.sh
+++ deps.sh
@@ -6,6 +6,6 @@ if ( command -v apt-get &> /dev/null ) ; then
     apt-get update -yqq && apt-get install -yqq build-essential sudo wget libelf-dev pkg-config zlib1g-dev libnuma-dev python3-pyelftools ninja-build meson linux-headers-$(uname -r) python3-pip
 elif ( command -v apk &> /dev/null ) ; then
     echo "Using apk"
-    apk add --no-cache  wget gcc libelf numactl python3 pkgconf zlib-dev py3-pip g++ py3-elftools autoconf build-base bsd-compat-headers linux-headers gcompat libstdc++
+    apk add --no-cache  wget gcc libelf numactl python3 pkgconf zlib-dev py3-pip g++ py3-elftools autoconf build-base bsd-compat-headers linux-headers gcompat libstdc++ perl-file-which
 fi
 
