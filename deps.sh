b47de41e77345ed3a20b0673bc61abe39c1605c5
Tom Barbette <tom.barbette@uclouvain.be>
2023-05-19 10:52:19 +0200

    Docker support
    Add missing header for musl
    Add options to change the destination march and keep the source
    Deps: Add usual dependencies like pcap, mlx5, bpf, etc
    sed: Do not use /Q which is non standard
    Delete all DPDK source right away, except files used by
    --enable-flow-api that are copied

--- deps.sh
+++ deps.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
+# This installs dependencies for both DPDK and FastClick, support for apt-get(Debian, Ubuntu, ...) and apk (Alpine) for now. PRs are welcome.
 
 echo "Installing dependencies..." ;
 if ( command -v apt-get &> /dev/null ) ; then
-    echo "Using apt-get"        
-    apt-get update -yqq && apt-get install -yqq build-essential sudo wget libelf-dev pkg-config zlib1g-dev libnuma-dev python3-pyelftools ninja-build meson linux-headers-$(uname -r) python3-pip
+    echo "Using apt-get"
+
+    apt-get update -yqq && apt-get install -yqq build-essential sudo wget libelf-dev pkg-config zlib1g-dev libnuma-dev python3-pyelftools ninja-build meson python3-pip libpcap-dev
+    header=linux-headers-$(uname -r)
+    if apt-cache search --names-only $header &> /dev/null ;  then
+       apt-get install -yqq $header
+    fi
 elif ( command -v apk &> /dev/null ) ; then
     echo "Using apk"
-    apk add --no-cache  wget gcc libelf numactl python3 pkgconf zlib-dev py3-pip g++ py3-elftools autoconf build-base bsd-compat-headers linux-headers gcompat libstdc++ perl-file-which
+    echo "@testing https://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+    apk add --no-cache  wget gcc libelf numactl python3 pkgconf zlib-dev py3-pip g++ py3-elftools autoconf build-base bsd-compat-headers linux-headers gcompat libstdc++ libpcap-dev libbpf-dev libxdp-dev
+    apk add --no-cache rdma-core-dev@testing
 fi
 
