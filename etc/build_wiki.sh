d053be60766609b28f51a95539d28a03233cc958
Tom Barbette <tom.barbette@uclouvain.be>
2023-07-28 09:03:55 +0200

    Add a script to build the wiki
    Will help to maintain it up-to-date

---
+++ etc/build_wiki.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./configure CXXFLAGS="-std=gnu++11" --enable-user-multithread --disable-verbose-batch --enable-simtime --disable-clone --enable-dpdk --enable-all-elements --enable-flow --enable-batch --enable-ctx --enable-cpu-load --enable-rsspp --enable-flow-api --with-netmap=../netmap/sys/ --enable-user-timestamp
+make -C doc install-man-markdown O=$(pwd)/../fastclick.wiki/elements
