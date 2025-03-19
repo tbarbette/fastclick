2256752a518e9e623005a5fe937070f6df533975
Rayan <90714316+Naya-01@users.noreply.github.com>
2025-03-19 12:58:01 +0100

    added MHD_OPTION_LISTENING_ADDRESS_REUSE to allow the reuse of the listening address for the HTTP server

--- elements/userlevel/httpserver.cc
+++ elements/userlevel/httpserver.cc
@@ -84,6 +84,7 @@ int HTTPServer::initialize(ErrorHandler *errh) {
             NULL,
             &ahc_echo,
             (void*)this,
+            MHD_OPTION_LISTENING_ADDRESS_REUSE, 1,
             MHD_OPTION_END);
     if (_daemon == NULL)
         return 1;
